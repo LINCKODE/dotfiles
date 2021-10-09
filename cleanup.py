9fc5371d48b1992f7210c0876de4f8480fc59088
LINCKODE <linckode@gmail.com>
2021-10-09 20:20:25 +0300

    update
    Signed-off-by: LINCKODE <linckode@gmail.com>

--- cleanup.py
+++ cleanup.py
@@ -2,9 +2,9 @@ import os
 
 print("WARNING: This will remove all the zsh files, oh my zsh and the vimrc!")
 
-consent = input("If you really wish to do this type >consent< : ")
+consent = input("If you really wish to do this type >consent<: ")
 
-if consent is "constent":
+if consent == "consent":
     print("Removing files...")
     os.system("rm -rdfv .zshfiles .zsh_history .zshrc .vimrc .oh-my-zsh/")
     print("Done.")
