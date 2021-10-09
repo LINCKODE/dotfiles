a1338af2680c080aa74a8d52e0904ae8d24f33a0
LINCKODE <linckode@gmail.com>
2021-10-09 20:28:08 +0300

    update
    Signed-off-by: LINCKODE <linckode@gmail.com>

--- cleanup.py
+++ cleanup.py
@@ -6,8 +6,13 @@ consent = input("If you really wish to do this type >consent<: ")
 
 if consent == "consent":
     print("Removing files...")
-    os.system("rm -rdfv .zshfiles .zsh_history .zshrc .vimrc .oh-my-zsh/")
+    os.system("cd .. && rm -rdfv .zshfiles .zsh_history .zshrc .vimrc .oh-my-zsh/ && cd -")
     print("Done.")
+
+    install = input("Do you wish to install the dotfiles? >yes<? ")
+    if install == "yes":
+        os.system("python3 install.py")
+
     exit()
 else:
     print("Exiting...")
